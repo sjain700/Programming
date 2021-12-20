1d03be1de0e98dc827645536d22938cfa1c8b4c7
sjain700 <sjain700@gmail.com>
2021-12-21 00:03:47 +0530

    Working O(n)

--- LeetCode/maximumUnitsOnATruck.cpp
+++ LeetCode/maximumUnitsOnATruck.cpp
@@ -1,3 +1,29 @@
-
-
-First Code to commit.
+class Solution {
+public:
+    
+    bool static sortcol(const vector<int> &v1,const vector<int> &v2)
+    {
+        return v1[1] > v2[1];
+    }
+        int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+         sort(boxTypes.begin(),boxTypes.end(),sortcol);
+        
+    
+            int ans = 0;
+            for(int i = 0; i < boxTypes.size() && truckSize > 0; i++)
+            {
+                if(truckSize >= boxTypes[i][0])
+                {
+                    ans = ans + boxTypes[i][1]*boxTypes[i][0];
+                    truckSize -= boxTypes[i][0];
+                }
+                else
+                {
+                    ans = ans + boxTypes[i][1]*truckSize;
+                    truckSize = 0;
+                }
+            }
+        
+        return ans;
+    }
+};
